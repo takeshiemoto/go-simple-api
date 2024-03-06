65bd60ef08808a614518fb78f1baa3972051a878
takeshiemoto <private.takeshiemoto@gmail.com>
2024-03-06 23:58:24 +0900

    055: refactor main function

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/takeshiemoto/go-simple-api
 
 go 1.21.1
+
+require golang.org/x/sync v0.6.0 // indirect
--- main.go
+++ main.go
@@ -1,17 +1,41 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
+	"golang.org/x/sync/errgroup"
+	"log"
 	"net/http"
-	"os"
 )
 
+// テスト容易性を上げるためにrun関数に切り出す
+// context.Contextは複数の関数、ゴルーチン間でキャンセルシグナルを伝播させる手段を提供する
+func run(ctx context.Context) error {
+	s := &http.Server{
+		Addr: ":18080",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+		}),
+	}
+	eg, ctx := errgroup.WithContext(ctx)
+	eg.Go(func() error {
+		if err := s.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Printf("failed to close: %+v", err)
+			return err
+		}
+		return nil
+	})
+
+	<-ctx.Done()
+	if err := s.Shutdown(context.Background()); err != nil {
+		log.Printf("failed to shutdown: %+v", err)
+	}
+	return eg.Wait()
+}
+
 func main() {
-	err := http.ListenAndServe(":18080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
-	}))
-	if err != nil {
-		fmt.Printf("failed to terminate server: %v", err)
-		os.Exit(1)
+	if err := run(context.Background()); err != nil {
+		log.Printf("failed to terminate server: %v", err)
 	}
 }
