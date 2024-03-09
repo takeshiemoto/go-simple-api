9aa9a8cf1e577b74b1f71eca72fd91378dde8049
takeshiemoto <private.takeshiemoto@gmail.com>
2024-03-10 07:48:56 +0900

    062 add server

---
+++ server.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"golang.org/x/sync/errgroup"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+type Server struct {
+	srv *http.Server
+	l   net.Listener
+}
+
+func NewServer(l net.Listener, mux http.Handler) *Server {
+	return &Server{
+		srv: &http.Server{Handler: mux},
+		l:   l,
+	}
+}
+
+// テスト容易性を上げるためにrun関数に切り出す
+// context.Contextは複数の関数、ゴルーチン間でキャンセルシグナルを伝播させる手段を提供する
+func (s *Server) Run(ctx context.Context) error {
+	// グレースフルシャットダウンの実装
+	// プロセス終了シグナルを受け取ったときに実行中の処理を正しく終了させる。
+	// シグナル(os.Interrupt, syscall.SIGTERM)を受け取ったらcontextをキャンセルする。
+	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	eg, ctx := errgroup.WithContext(ctx)
+	eg.Go(func() error {
+		if err := s.srv.Serve(s.l); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			// 指定されたフォーマットでログに出力する
+			log.Printf("failed to close: %+v", err)
+			return err
+		}
+		return nil
+	})
+
+	<-ctx.Done()
+	if err := s.srv.Shutdown(context.Background()); err != nil {
+		log.Printf("failed to shutdown: %+v", err)
+	}
+	// グレースフルシャットダウンの終了を待つ
+	return eg.Wait()
+}
--- server_test.go
+++ server_test.go
@@ -10,16 +10,19 @@ import (
 	"testing"
 )
 
-func TestRun(t *testing.T) {
-	t.Skip("リファクタリング中...")
+func TestServer_Run(t *testing.T) {
 	l, err := net.Listen("tcp", "localhost:0")
 	if err != nil {
 		t.Fatalf("failed to listen port %v", err)
 	}
 	ctx, cancel := context.WithCancel(context.Background())
 	eg, ctx := errgroup.WithContext(ctx)
+	mux := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+	})
 	eg.Go(func() error {
-		return run(ctx)
+		s := NewServer(l, mux)
+		return s.Run(ctx)
 	})
 
 	in := "message"
