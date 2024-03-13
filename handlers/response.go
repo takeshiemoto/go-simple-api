13c4e3e6c7b3be82143f0150894b74ccfa2729d0
takeshiemoto <private.takeshiemoto@gmail.com>
2024-03-13 23:23:46 +0900

    066 add helper

---
+++ handlers/response.go
@@ -0,0 +1,34 @@
+package handlers
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type ErrorResponse struct {
+	Message string   `json:"message"`
+	Details []string `json:"details,omitempty"`
+}
+
+func ResponseJSON(ctx context.Context, w http.ResponseWriter, body any, status int) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	bodyBytes, err := json.Marshal(body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		resp := ErrorResponse{
+			Message: http.StatusText(http.StatusInternalServerError),
+		}
+		if err := json.NewEncoder(w).Encode(resp); err != nil {
+			fmt.Printf("write error response error: %v", err)
+		}
+		return
+	}
+
+	w.WriteHeader(status)
+	if _, err := fmt.Fprintf(w, "%s", bodyBytes); err != nil {
+		fmt.Printf("write response error: %v", err)
+	}
+
+}
