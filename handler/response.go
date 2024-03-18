66f658eb8ef2037afdfc49d3d5661e8805805a54
takeshiemoto <private.takeshiemoto@gmail.com>
2024-03-18 23:10:44 +0900

    067 add task comment

--- handler/response.go
+++ handler/response.go
@@ -7,13 +7,17 @@ import (
 	"net/http"
 )
 
+// ErrorResponse はエラーレスポンスの構造体
 type ErrorResponse struct {
 	Message string   `json:"message"`
 	Details []string `json:"details,omitempty"`
 }
 
+// ResponseJSON はJSON形式のレスポンスを生成する
 func ResponseJSON(ctx context.Context, w http.ResponseWriter, body any, status int) {
+	// レスポンスのContent-typeを設定
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	// bodyをJSON形式に変換
 	bodyBytes, err := json.Marshal(body)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
@@ -26,9 +30,10 @@ func ResponseJSON(ctx context.Context, w http.ResponseWriter, body any, status i
 		return
 	}
 
+	// ステータスコードを指定
 	w.WriteHeader(status)
+	// レスポンスボディをResponseWriterに書き込む
 	if _, err := fmt.Fprintf(w, "%s", bodyBytes); err != nil {
 		fmt.Printf("write response error: %v", err)
 	}
-
 }
