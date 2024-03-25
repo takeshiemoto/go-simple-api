b4496c45cc1f46d69af6d8db0ef85163f0d97a5a
takeshiemoto <takeshi.emoto@chatwork.com>
2024-03-25 17:13:03 +0900

    add comments

--- store/repository.go
+++ store/repository.go
@@ -12,34 +12,48 @@ import (
 	"github.com/takeshiemoto/go-simple-api/config"
 )
 
+// New はデータベースの接続を行う関数
 func New(ctx context.Context, cfg *config.Config) (*sqlx.DB, func(), error) {
 	// fmt.Sprintfは指定されたフォーマットで文字列を生成する
+	// parseTimeはtime.Time型のフィールドに正しい値をセットするために必要
 	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?parseTime=true", cfg.DBUser, cfg.DBPassword, cfg.DBHost, cfg.DBPort, cfg.DBName))
 	if err != nil {
 		return nil, nil, err
 	}
 
+	// コンテキストにタイムアウトを設定する
 	ctx, cancel := context.WithTimeout(ctx, 2*time.Second)
 	defer cancel()
+
+	// PingContextはデータベースへの接続を確認する
 	if err := db.PingContext(ctx); err != nil {
 		return nil, func() {
+			// Pingに失敗した場合はデータベースの接続を閉じる
 			_ = db.Close()
 		}, err
 	}
+	// 接続を作成する
 	xdb := sqlx.NewDb(db, "mysql")
 	return xdb, func() { _ = db.Close() }, nil
 }
 
+// Beginner はトランザクションを開始するインターフェース
 type Beginner interface {
+	// BeginTx はトランザクションを開始するために使われる
 	BeginTx(ctx context.Context, opts *sql.TxOptions) (*sql.Tx, error)
 }
 
+// Preparer はSQLステートメントを準備するインターフェース
 type Preparer interface {
+	// PreparexContext はSQLステートメントを準備する
 	PreparexContext(ctx context.Context, query string) (*sqlx.Stmt, error)
 }
 
+// Execer はSQLステートメントを実行するインターフェース
 type Execer interface {
+	// ExecContext はSQLステートメントを実行する
 	ExecContext(ctx context.Context, query string, args ...any) (sql.Result, error)
+	// NamedExecContext は名前付きSQLステートメントを実行する
 	NamedExecContext(ctx context.Context, query string, arg interface{}) (sql.Result, error)
 }
 
